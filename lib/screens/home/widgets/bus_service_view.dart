31c7d126a7669643969bef11c00a725f3faaf6e8
Leobelle Joseph Villocog <leobellejosephvillocog@outlook.com>
2021-07-24 21:01:16 +0800

    Beta Release - used flutter staggered grid view for bus services. https://pub.dev/packages/flutter_staggered_grid_view

--- lib/screens/home/widgets/bus_service_view.dart
+++ lib/screens/home/widgets/bus_service_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:my_bus/blocs/blocs.dart';
 import 'package:my_bus/models/models.dart';
 import 'package:my_bus/screens/bus_route/bus_route_screen.dart';
@@ -18,10 +19,11 @@ class BusServiceView extends StatelessWidget {
         } else if (state.status == BusDataStatus.no_internet) {
           return CenteredText(text: 'No internet. Please check connection.');
         } else {
-          return GridView.builder(
+          return StaggeredGridView.countBuilder(
               itemCount: state.serviceData.length,
-              gridDelegate:
-                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              mainAxisSpacing: 2,
+              crossAxisSpacing: 2,
+              crossAxisCount: 4,
               itemBuilder: (context, index) {
                 final item = state.serviceData[index];
                 final BusOperator operator = item.busOperator;
@@ -90,9 +92,84 @@ class BusServiceView extends StatelessWidget {
                     ),
                   ),
                 );
-              });
+              },
+              staggeredTileBuilder: (index) =>
+                  StaggeredTile.count(2, index.isEven ? 2 : 1));
         }
       },
     );
   }
 }
+// return GridView.builder(
+// itemCount: state.serviceData.length,
+// gridDelegate:
+// SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+// itemBuilder: (context, index) {
+// final item = state.serviceData[index];
+// final BusOperator operator = item.busOperator;
+// return Padding(
+// padding: const EdgeInsets.all(5.0),
+// child: RawMaterialButton(
+// onPressed: () {
+// context
+//     .read<BusRouteCubit>()
+//     .fetchRoute(service: item.serviceNo);
+// showModalBottomSheet(
+// backgroundColor: Colors.white,
+// elevation: 2,
+// context: context,
+// builder: (context) {
+// return BusRouteScreen(
+// service: item.serviceNo, code: '');
+// },
+// );
+// },
+// highlightColor: operator.color,
+// shape: RoundedRectangleBorder(
+// borderRadius: BorderRadius.circular(3),
+// ),
+// child: Container(
+// padding: const EdgeInsets.all(5),
+// decoration: BoxDecoration(
+// borderRadius: BorderRadius.circular(3),
+// gradient: LinearGradient(
+// begin: Alignment.topCenter,
+// end: Alignment.bottomCenter,
+// colors: [
+// operator.color.withOpacity(0.9),
+// operator.color.withOpacity(0.3),
+// operator.color.withOpacity(0.2),
+// ]),
+// ),
+// child: Column(
+// mainAxisSize: MainAxisSize.max,
+// crossAxisAlignment: CrossAxisAlignment.stretch,
+// children: [
+// Expanded(
+// flex: 1,
+// child: Text(
+// operator.name,
+// style: TextStyle(
+// color: operator.textColor,
+// fontWeight: FontWeight.w600),
+// ),
+// ),
+// const Divider(color: Colors.grey, height: 0),
+// Expanded(
+// flex: 4,
+// child: Center(
+// child: Text(
+// item.serviceNo,
+// style: const TextStyle(
+// fontSize: 35,
+// color: Colors.black54,
+// fontWeight: FontWeight.w600),
+// ),
+// ),
+// ),
+// ],
+// ),
+// ),
+// ),
+// );
+// });
