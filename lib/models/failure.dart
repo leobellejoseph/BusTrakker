cf8b5ee3fc0a7ae62d2042b5636bab443656f027
Leobelle Joseph Villocog <leobellejosephvillocog@outlook.com>
2021-10-09 13:02:22 +0800

    added factory constructors

--- lib/models/failure.dart
+++ lib/models/failure.dart
@@ -1,8 +1,10 @@
 class Failure {
   final String code;
   final String message;
-
-  Failure({required this.code, required this.message});
-
-  factory Failure.none() => Failure(code: '', message: '');
+  const Failure({required this.code, required this.message});
+  factory Failure.none() => const Failure(code: '', message: '');
+  factory Failure.arrival() => const Failure(
+      code: 'Bus Arrival', message: 'Failed to fetch Bus Arrival');
+  factory Failure.arrivals() => const Failure(
+      code: 'Bus Arrival', message: 'Failed to fetch Bus Arrivals Services');
 }
